42cba124ab56768bf92ba8c54d277d724b68cfbe
Nathan Gill <nathan.j.gill@outlook.com>
2026-02-14 15:51:19 +0000

    toggle servos in order

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,50 @@
 #include <Arduino.h>
+#include <Servo.h>
 
-const int LED_PIN = LED_BUILTIN;
+Servo servos[4];
+int next_drop = 0;
+
+void setup_servos() {
+    servos[0].attach(D0);
+    servos[0].write(0);
+
+    servos[1].attach(D1);
+    servos[1].write(0);
+
+    servos[2].attach(D2);
+    servos[2].write(0);
+
+    servos[3].attach(D3);
+    servos[3].write(0);
+
+    delay(1000);
+}
+
+void activate_servo(Servo *servo) {
+    servo->write(180);
+    delay(1000);
+    servo->write(0);
+    delay(500);
+}
+
+void drop_cube() {
+    if (next_drop >= 4)
+        return;
+
+    activate_servo(&servos[next_drop]);
+    next_drop++;
+}
+
+void reset() {
+    next_drop = 0;
+}
 
 void setup() {
-    Serial.begin(9600);
-    Serial.println("ESP-12E LED Blink Test");
-
-    pinMode(LED_PIN, OUTPUT);
+    setup_servos();
+    reset();
 }
 
 void loop() {
-    digitalWrite(LED_PIN, LOW);
-    Serial.println("LED ON");
-    delay(1000);
-
-    digitalWrite(LED_PIN, HIGH);
-    Serial.println("LED OFF");
-    delay(1000);
+    drop_cube();
+    delay(3000);
 }
